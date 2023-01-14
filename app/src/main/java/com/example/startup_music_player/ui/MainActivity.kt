8e302b2720ff238fbf54da7b0ced642f787ff250
user <samankhosravi09@gmail.com>
2023-01-15 00:17:02 +0330

    first commit

---
+++ app/src/main/java/com/example/startup_music_player/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.startup_music_player.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.startup_music_player.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
