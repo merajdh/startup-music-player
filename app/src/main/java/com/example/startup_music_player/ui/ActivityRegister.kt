12ccc4f596efba430d2c2d4ad8ef865644d37128
user <samankhosravi09@gmail.com>
2023-01-16 21:32:37 +0330

    Add Home class

--- app/src/main/java/com/example/startup_music_player/ui/ActivityRegister.kt
+++ app/src/main/java/com/example/startup_music_player/ui/ActivityRegister.kt
@@ -2,13 +2,27 @@ package com.example.startup_music_player.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.startup_music_player.databinding.ActivityRegisterBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ActivityRegister : AppCompatActivity() {
-    lateinit var  binding: ActivityRegisterBinding
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnRegister.setOnClickListener { IschektRegister() }
+    }
+
+    private fun IschektRegister() {
+        if (binding.EdtUser.text.isNotEmpty() &&
+            binding.EdtGmail.text.isNotEmpty() &&
+            binding.EdtNumber.text.isNotEmpty() &&
+            binding.EdtPassword.text.isNotEmpty()) {
+
+        }else{
+
+        }
     }
 }
--- app/src/main/java/com/example/startup_music_player/ui/MainActivity.kt
+++ app/src/main/java/com/example/startup_music_player/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.startup_music_player.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.startup_music_player.R
 import com.example.startup_music_player.databinding.ActivityMainBinding
 
@@ -12,6 +14,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.animLogin.playAnimation()
+        binding.txtRegister.setOnClickListener {
+            val intent = Intent(this, ActivityRegister::class.java)
+            startActivity(intent)
+        }
+        binding.btnLogin.setOnClickListener { IschektLogin() }
 
+    }
+
+    private fun IschektLogin() {
+        // Chekt isNotEmpty  EDT
+        if (binding.EdtUserLogin.text.isNotEmpty() &&
+            binding.EdtPasswordLogin.text.isNotEmpty()
+        ){
+            val intent = Intent(this, ActivityHome::class.java)
+            startActivity(intent)
+        } else {
+
+        }
     }
 }
