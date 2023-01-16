18f30cb848b2e959672d64a3da83f234f784753d
user <samankhosravi09@gmail.com>
2023-01-16 14:31:36 +0330

    Add Login Page

--- app/src/main/java/com/example/startup_music_player/ui/MainActivity.kt
+++ app/src/main/java/com/example/startup_music_player/ui/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.startup_music_player.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.startup_music_player.R
+import com.example.startup_music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.animLogin.playAnimation()
+
     }
 }
