fd04a639fc3fd7dfd36d1de4ca9e60104612ede6
Meraj_Dh <m.dahmardeh16@gmail.com>
2023-01-17 13:22:36 +0330

    add Snackbar

--- app/src/main/java/com/example/startup_music_player/ui/ActivityRegister.kt
+++ app/src/main/java/com/example/startup_music_player/ui/ActivityRegister.kt
@@ -3,6 +3,8 @@ package com.example.startup_music_player.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.startup_music_player.R
 import com.example.startup_music_player.databinding.ActivityRegisterBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -13,6 +15,9 @@ class ActivityRegister : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnRegister.setOnClickListener { IschektRegister() }
+
+
+
     }
 
     private fun IschektRegister() {
@@ -21,8 +26,20 @@ class ActivityRegister : AppCompatActivity() {
             binding.EdtNumber.text.isNotEmpty() &&
             binding.EdtPassword.text.isNotEmpty()) {
 
-        }else{
-
+        }else if (binding.EdtUser.text.isEmpty() or
+            binding.EdtGmail.text.isEmpty() or
+            binding.EdtNumber.text.isEmpty() or
+            binding.EdtPassword.text.isEmpty()){
+            SnackbarEmpty()
         }
+
     }
+    fun SnackbarEmpty (){
+        Snackbar
+            .make(binding.root , "لطفا تمام فیلد ها را تکمیل کنید !" , Snackbar.LENGTH_LONG)
+            .setBackgroundTint(ContextCompat.getColor(this , R.color.light_blue))
+            .setTextColor(ContextCompat.getColor(this , R.color.white))
+            .show()
+    }
+
 }
