178edc600ce0f8f3fbd364d68907fb21c9b24da1
user <samankhosravi09@gmail.com>
2023-01-19 11:05:34 +0330

    Update btnmnavigation And add Slider

--- app/src/main/java/com/example/startup_music_player/ui/MainActivity.kt
+++ app/src/main/java/com/example/startup_music_player/ui/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.startup_music_player.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.toColor
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.startup_music_player.R
 import com.example.startup_music_player.databinding.ActivityMainBinding
 
@@ -15,28 +19,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         SelectedbtnNavigeyshen() // BtnNavigeyshen
+        AddsliderCod() // slider
     }
+
+
     private fun SelectedbtnNavigeyshen() {
-        binding.btnNavigeyshen.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> {
-                    binding.selectedCategory.visibility = View.INVISIBLE
-                    binding.selectedProfile.visibility = View.INVISIBLE
-                    binding.selectedHome.visibility = View.VISIBLE
+        binding.butennavigetion.setItemSelected( R.id.item1)
+        binding.butennavigetion.setOnItemSelectedListener {
+            when(it){
+                R.id.item0 -> {
+
                 }
-                R.id.category -> {
-                    binding.selectedHome.visibility = View.INVISIBLE
-                    binding.selectedProfile.visibility = View.INVISIBLE
-                    binding.selectedCategory.visibility = View.VISIBLE
+                R.id.item1 -> {
+
                 }
-                R.id.Profile -> {
-                    binding.selectedHome.visibility = View.INVISIBLE
-                    binding.selectedCategory.visibility = View.INVISIBLE
-                    binding.selectedProfile.visibility = View.VISIBLE
+                R.id.item2 -> {
+
                 }
             }
 
-            true
         }
     }
+
+
+    private fun AddsliderCod() {
+        val ItemSlider = ArrayList<SlideModel>()
+        ItemSlider.add(SlideModel("https://images.hdqwalls.com/download/eminem-rapper-wallpaper-2560x1440.jpg"))
+        ItemSlider.add(SlideModel("https://images4.alphacoders.com/193/193108.jpg"))
+        ItemSlider.add(SlideModel("https://wallpaper.dog/large/17006205.jpg"))
+        binding.slider.setImageList(ItemSlider)
+
+    }
+
 }
