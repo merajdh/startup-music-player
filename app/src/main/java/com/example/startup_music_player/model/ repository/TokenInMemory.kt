e41a9f39caecdc17dd9efc5296dcef1ce10ff62c
user <samankhosravi09@gmail.com>
2023-01-29 22:45:37 +0330

    add TokenInMemory

---
+++ app/src/main/java/com/example/startup_music_player/model/ repository/TokenInMemory.kt
@@ -0,0 +1,14 @@
+package com.example.startup_music_player.model.repository
+
+object TokenInMemory {
+    var username : String? =null
+    private set
+
+    var Token :String? =null
+    private set
+
+    fun refreshToken(Username:String , newToken:String){
+        this.username = Username
+        this.Token = newToken
+    }
+}
