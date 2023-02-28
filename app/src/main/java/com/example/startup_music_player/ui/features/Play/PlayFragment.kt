869247e4ec9677bd01fd27acefba0a7f0f6d4dff
Meraj_Dh <m.dahmardeh16@gmail.com>
2023-02-28 20:30:36 +0330

    Add Detail of Play

--- app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
+++ app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
@@ -4,12 +4,12 @@ import android.graphics.RenderEffect
 import android.graphics.Shader
 import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.startup_music_player.R
+import androidx.fragment.app.Fragment
 import com.example.startup_music_player.databinding.FragmentPlayBinding
+import com.example.startup_music_player.ui.features.playDetail.playDetailFragment
 
 class PlayFragment : Fragment() {
     lateinit var binding: FragmentPlayBinding
@@ -19,13 +19,28 @@ class PlayFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentPlayBinding.inflate(layoutInflater, container, false)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            binding.imgBackground.setRenderEffect(RenderEffect.createBlurEffect(30f, 30f , Shader.TileMode.MIRROR))
-        }
+        setBlur()
+        setOnClickListeners()
         return binding.root
 
     }
 
+
+    private fun setBlur(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            binding.imgBackground.setRenderEffect(RenderEffect.createBlurEffect(30f, 30f , Shader.TileMode.MIRROR))
+        }
+    }
+
+    private fun setOnClickListeners() {
+
+        binding.btnOpenMore.setOnClickListener {
+            val transform = playDetailFragment()
+            transform.isCancelable = true
+            transform.show(parentFragmentManager, null)
+        }
+
+
+    }
 
 }
