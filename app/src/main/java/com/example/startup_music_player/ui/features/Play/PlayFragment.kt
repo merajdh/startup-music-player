d74fe52ecc654fad70950600ffedf9928a086079
Meraj_Dh <m.dahmardeh16@gmail.com>
2023-03-01 19:43:50 +0330

    fix play Fragment and Add Transform Fragments Animation

--- app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
+++ app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
@@ -1,15 +1,20 @@
 package com.example.startup_music_player.ui.features.Play
 
-import android.graphics.RenderEffect
-import android.graphics.Shader
-import android.os.Build
+
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.startup_music_player.databinding.FragmentPlayBinding
+import com.example.startup_music_player.ui.features.Detail.DetaiMusiclFragment
 import com.example.startup_music_player.ui.features.playDetail.playDetailFragment
+import jp.wasabeef.glide.transformations.BlurTransformation
+
 
 class PlayFragment : Fragment() {
     lateinit var binding: FragmentPlayBinding
@@ -19,21 +24,61 @@ class PlayFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentPlayBinding.inflate(layoutInflater, container, false)
-        setBlur()
+        blurImage()
+
         setOnClickListeners()
         return binding.root
 
     }
 
+        private fun blurImage() {
+            Glide.with(this).load(binding.imgBackground.drawable)
+                .apply(RequestOptions.bitmapTransform(BlurTransformation(8, 1)))
+                .into(binding.imgBackground)        }
 
-    private fun setBlur(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            binding.imgBackground.setRenderEffect(RenderEffect.createBlurEffect(30f, 30f , Shader.TileMode.MIRROR))
-        }
-    }
 
     private fun setOnClickListeners() {
 
+        //music
+
+        // previous ->
+        binding.btnPrevious.setOnClickListener {
+
+        }
+
+        //  next ->
+        binding.btnNext.setOnClickListener {
+
+        }
+
+        // play ->
+        binding.btnPlay.setOnClickListener {
+
+        }
+
+        //another
+
+        // more button  ->
+        binding.btnMore.setOnClickListener {
+            val transform = DetaiMusiclFragment()
+            transform.isCancelable = true
+            transform.show(parentFragmentManager , null)
+        }
+
+        // like ->
+        binding.btnLiked.setOnClickListener {
+
+            if (binding.btnLiked.isChecked){
+                //liked ->
+            }
+            else {
+                //disliked - >
+
+            }
+
+        }
+
+        // show more data ->
         binding.btnOpenMore.setOnClickListener {
             val transform = playDetailFragment()
             transform.isCancelable = true
