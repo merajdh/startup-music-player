98e51af1d0a3765fb255d3a513676198e9e8a88d
Meraj_Dh <m.dahmardeh16@gmail.com>
2023-03-24 01:30:32 +0430

    Set Category Data

--- app/src/main/java/com/example/startup_music_player/ui/features/Category/CategoryFragment.kt
+++ app/src/main/java/com/example/startup_music_player/ui/features/Category/CategoryFragment.kt
@@ -8,17 +8,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.startup_music_player.databinding.FragmentCategoryBinding
+import com.example.startup_music_player.model.Adapter.CategoryAdapterTypeMusic
+import com.example.startup_music_player.model.Adapter.HomeAdapterHappyMusic
+import com.example.startup_music_player.model.Adapter.OnClickCategory
+import com.example.startup_music_player.model.Adapter.OnClickHome
+import com.example.startup_music_player.model.Contract.ContractCategory
+import com.example.startup_music_player.model.data.CategoryRespomse
 import com.example.startup_music_player.model.myApp.myApp
+import com.example.startup_music_player.model.net.createApiService
+import com.example.startup_music_player.model.presenter.PresenterCategory
+import com.example.startup_music_player.model.presenter.PresenterPlayMusic
+import com.example.startup_music_player.util.NetworkChecker
 
-class CategoryFragment : Fragment() {
+class CategoryFragment : Fragment() , ContractCategory.View , OnClickCategory {
     lateinit var binding: FragmentCategoryBinding
+    lateinit var presenter: ContractCategory.PresenterCategory
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentCategoryBinding.inflate(layoutInflater,container,false)
         setOnClickListeners()
         MoreClickListener()
+        presenter = PresenterCategory(createApiService(), NetworkChecker(binding.root.context).isInternetConnected)
+
+        lifecycleScope.launchWhenCreated {
+            presenter.OnAttach(this@CategoryFragment)
+
+        }
         return binding.root
     }
 
@@ -66,6 +87,19 @@ class CategoryFragment : Fragment() {
 
 
         }
+    }
+
+    override fun TypeOfMusic(data: List<CategoryRespomse>) {
+
+        val adapter = CategoryAdapterTypeMusic(data,this)
+        binding.mouduleOneCategory.recType.layoutManager =
+            GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        binding.mouduleOneCategory.recType.adapter = adapter
+
+    }
+
+    override fun ClickCategory(data: CategoryRespomse) {
+
     }
 
 }
--- app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
+++ app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
@@ -5,19 +5,24 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.startup_music_player.databinding.FragmentPlayBinding
 import com.example.startup_music_player.model.Contract.ContractPlayMusic
+import com.example.startup_music_player.model.data.MusicDetail
 import com.example.startup_music_player.model.data.MusicRespomse
 import com.example.startup_music_player.model.net.createApiService
 import com.example.startup_music_player.model.presenter.PresenterPlayMusic
 import com.example.startup_music_player.ui.features.playDetail.playDetailFragment
 import com.example.startup_music_player.util.NetworkChecker
+import com.squareup.picasso.Picasso
+import ir.dunijet.dunibazaar.util.BASE_URL
 import jp.wasabeef.glide.transformations.BlurTransformation
 
+const val BASE_URL_IMAGE = "https://abolfazlzzx13.pythonanywhere.com/"
 class PlayFragment : Fragment(), ContractPlayMusic.View {
     lateinit var binding: FragmentPlayBinding
     lateinit var presenter: ContractPlayMusic.Presenter
@@ -27,7 +32,6 @@ class PlayFragment : Fragment(), ContractPlayMusic.View {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentPlayBinding.inflate(layoutInflater, container, false)
-        blurImage()
         setOnClickListeners()
         presenter = PresenterPlayMusic(createApiService(), NetworkChecker(binding.root.context).isInternetConnected)
 
@@ -41,10 +45,11 @@ class PlayFragment : Fragment(), ContractPlayMusic.View {
     }
 
 
-    private fun blurImage() {
+    private fun blurImage(view: ImageView) {
         Glide.with(this).load(binding.imgBackground.drawable)
             .apply(RequestOptions.bitmapTransform(BlurTransformation(8, 1)))
-            .into(binding.imgBackground)}
+            .into(view)
+    }
 
     private fun setOnClickListeners() {
 
@@ -57,8 +62,23 @@ class PlayFragment : Fragment(), ContractPlayMusic.View {
 
     }
 
-    override fun PlayMusic(Music: MusicRespomse) {
-        Log.v("eeee2",Music.toString())
+    override fun PlayMusic(Music: MusicDetail) {
+        val url =
+            "https://dkstatics-public.digikala.com/digikala-products/fb3e51269c9931fc223047e2a9b063d54224e6f0_1651833531.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90"
+
+        Glide .with(this).load(BASE_URL_IMAGE + Music.cover)
+            .into(binding.imgCover)
+
+        Log.v("123" , Music.cover)
+        Glide .with(this).load(BASE_URL_IMAGE + Music.cover)
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(8, 4)))
+            .into(binding.imgBackground)
+
+
+        binding.Pliy.text = Music.artist[0].name
+
+        binding.txtNameMusic.text = Music.title
+
     }
 
 }
