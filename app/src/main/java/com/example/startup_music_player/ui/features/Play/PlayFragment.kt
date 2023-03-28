5c05b00e1415c9a22f93694cfc891970ac7ca298
user <samankhosravi09@gmail.com>
2023-03-28 10:20:07 +0330

    Coding Ply Music

--- app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
+++ app/src/main/java/com/example/startup_music_player/ui/features/Play/PlayFragment.kt
@@ -1,5 +1,7 @@
 package com.example.startup_music_player.ui.features.Play
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,18 +11,28 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.startup_music_player.R
 import com.example.startup_music_player.databinding.FragmentPlayBinding
 import com.example.startup_music_player.model.Contract.ContractPlayMusic
 import com.example.startup_music_player.model.data.MusicDetail
 import com.example.startup_music_player.model.net.createApiService
 import com.example.startup_music_player.model.presenter.PresenterPlayMusic
+import com.example.startup_music_player.ui.features.Main.MainFragment
 import com.example.startup_music_player.ui.features.playDetail.playDetailFragment
 import com.example.startup_music_player.util.NetworkChecker
+import com.google.android.material.slider.Slider
 import jp.wasabeef.glide.transformations.BlurTransformation
+import java.util.*
+import kotlin.concurrent.schedule
 
 class PlayFragment : Fragment(), ContractPlayMusic.View {
+
     lateinit var binding: FragmentPlayBinding
     lateinit var presenter: ContractPlayMusic.Presenter
+    lateinit var mediaParser: MediaPlayer
+    lateinit var timer: Timer
+    var ispluing = false
+    var taghir = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,11 +41,25 @@ class PlayFragment : Fragment(), ContractPlayMusic.View {
         binding = FragmentPlayBinding.inflate(layoutInflater, container, false)
         blurImage()
         setOnClickListeners()
-
+        binding.Pliy.setOnClickListener { plymusic() }
         binding.viewMain.visibility = View.GONE
         binding.shimmerPlay.startShimmer()
         presenter = PresenterPlayMusic(createApiService(), NetworkChecker(binding.root.context).isInternetConnected)
+        binding.slider.addOnChangeListener { slider, value, fromUser ->
 
+            binding.start.text = converttimemusic(value.toLong())
+            taghir = fromUser
+
+        }
+        binding.slider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener{
+            override fun onStartTrackingTouch(slider: Slider) {
+
+            }
+
+            override fun onStopTrackingTouch(slider: Slider) {
+                mediaParser.seekTo(slider.value.toInt())
+            }
+        })
 
         lifecycleScope.launchWhenCreated {
             presenter.OnAttach(this@PlayFragment)
@@ -41,6 +67,18 @@ class PlayFragment : Fragment(), ContractPlayMusic.View {
         }
         return binding.root
 
+    }
+
+    private fun plymusic() {
+        if (ispluing){
+            mediaParser.pause()
+            binding.ImgPlay.setImageResource(R.drawable.ic_play)
+            ispluing = false
+        }else{
+            mediaParser.start()
+            binding.ImgPlay.setImageResource(R.drawable.ic_pause)
+            ispluing = true
+        }
     }
 
 
@@ -61,21 +99,34 @@ class PlayFragment : Fragment(), ContractPlayMusic.View {
     }
 
     override fun PlayMusic(Music: MusicDetail) {
-
+        mediaParser = MediaPlayer.create(binding.root.context, Uri.parse(Music.url))
+        mediaParser.start()
+        ispluing = true
+        binding.ImgPlay.setImageResource(R.drawable.ic_pause)
+        binding.slider.valueTo= mediaParser.duration.toFloat()
+        binding.taim.text = converttimemusic(mediaParser.duration.toLong())
+        mediaParser.isLooping = true
+        timer = Timer()
+        timer.schedule(1000) {
+            binding.slider.value = mediaParser.currentPosition.toFloat()
+        }
         Glide .with(this).load(Music.cover)
             .into(binding.imgCover)
         Log.v("123" , Music.cover)
         Glide .with(this).load( Music.cover)
             .apply(RequestOptions.bitmapTransform(BlurTransformation(8, 4)))
             .into(binding.imgBackground)
-
         binding.shimmerPlay.visibility = View.GONE
         binding.viewMain.visibility = View.VISIBLE
 
         binding.Pliy.text = Music.artist[0].name
-
         binding.txtNameMusic.text = Music.title
         Log.v("eeee2",Music.toString())
     }
+    fun converttimemusic(duration: Long) : String{
+        val secend = duration / 1000 % 60
+        val minit = duration / (1000 * 60)
+        return java.lang.String.format(Locale.US , "%02d:%02d", minit , secend)
 
+    }
 }
